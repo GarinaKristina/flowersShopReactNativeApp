73fa74dd83a8c95ea6bfb989c683f40fdc4db0cc
Kristina Garina <Kristina.Garina@coxautoinc.com>
2023-07-03 16:49:06 +0300

    add

--- App.js
+++ App.js
@@ -2,15 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View , Button, Alert} from 'react-native';
 
 export default function App() {
-  const textInConsole = () => Alert.alert("Look at you","You are beauty", [
-    {text:"Yeeeeeep"},
-    {text:"Why?"}
-  ])
-  const textAfterPressButton = () => Alert.alert('МОЛОДЦОМ!')
+  // const textInConsole = () => Alert.alert("Look at you","You are beauty", [
+  //   {text:"Yeeeeeep"},
+  //   {text:"Why?"}
+  // ])
+  const textAfterPressButton = () => Alert.prompt('Here we go.....')
   return (
     <View style={styles.container}>
-      <Text numberOfLines={2} style={styles.text} onPress={textInConsole}>Kristina {'\n'}the best of the best </Text>
-      <Button title={'Ну кликни на меня'} color='black' onPress={textAfterPressButton} />
+      <Text numberOfLines={1} style={styles.text} >Welcome!!! </Text>
+      <Text numberOfLines={1} style={styles.text} >You are in a flower shop </Text>
+      <Text numberOfLines={1} style={styles.text} >If you want to know our range </Text>
+      
+      <Button title={'Click on me'} color='black' onPress={textAfterPressButton} />
 
       <StatusBar style="auto" />
     </View>
@@ -22,9 +25,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff1',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
   text: {
-    color: 'red'
+    color: 'black'
   },
 });

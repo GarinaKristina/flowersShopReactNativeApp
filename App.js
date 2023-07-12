efbb9a23f3d193301a11406ae065f8a9c2df4039
Kristina Garina <Kristina.Garina@coxautoinc.com>
2023-07-12 17:56:50 +0300

    add picture and button

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Button, Alert} from 'react-native';
+import { StyleSheet, Text, View , Button, Alert, Image, TouchableOpacity} from 'react-native';
 
 export default function App() {
-  // const textInConsole = () => Alert.alert("Look at you","You are beauty", [
-  //   {text:"Yeeeeeep"},
-  //   {text:"Why?"}
-  // ])
+
   const textAfterPressButton = () => Alert.prompt('Here we go.....')
   return (
     <View style={styles.container}>
-      <Text numberOfLines={1} style={styles.text} >Welcome!!! </Text>
-      <Text numberOfLines={1} style={styles.text} >You are in a flower shop </Text>
-      <Text numberOfLines={1} style={styles.text} >If you want to know our range </Text>
-      
-      <Button title={'Click on me'} color='black' onPress={textAfterPressButton} />
-
+      <TouchableOpacity style={styles.buttonStyle}>
+      <Text style={styles.buttonText}>Button</Text>
+    </TouchableOpacity>
+      <Image style={styles.mainPicture} source={require('./assets/flowers.png')} />      
       <StatusBar style="auto" />
     </View>
   );
@@ -23,11 +18,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff1',
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'center',
   },
   text: {
     color: 'black'
+  },
+  mainPicture: {
+  flex: 1,
+  width: '100%',
+  height: '100%',
+  resizeMode: 'cover',
+  },
+  buttonStyle:{
+    backgroundColor: "black",
+    padding: 100,
+    position: 'absolute',
+    top: 300,
+    left: 'center',
+    borderRadius: 40,
+    elevation: 2,  
+    zIndex: 1
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });

6846a0b7f1331e30779ce03d8fc7185d200fb48b
Kristina Garina <Kristina.Garina@coxautoinc.com>
2023-09-25 10:45:29 +0300

    add

--- components/CatalogPage.js
+++ components/CatalogPage.js
@@ -1,18 +1,17 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  Alert,
-  Image,
-  TouchableOpacity,
-} from "react-native";
-
-import flowers from "../assets/magazin-cvetov.png";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import flowersCatalog from "../assets/magazin-cvetov.png";
 
 export default function CatalogPage() {
+  const loadScene = () => {
+    navigation.navigate("");
+  };
   return (
     <View style={styles.container}>
-      <Image source={flowers} style={styles.mainPicture} />
+      <Text>Catalog</Text>
+      <TouchableOpacity style={styles.buttonStyle} onPress={loadScene}>
+        <Text style={styles.buttonText}>Roses</Text>
+      </TouchableOpacity>
+      <Image source={flowersCatalog} style={styles.mainPicture} />
     </View>
   );
 }
@@ -25,5 +24,25 @@ const styles = StyleSheet.create({
   mainPicture: {
     flex: 1,
     resizeMode: "cover",
+  },
+  buttonStyle: {
+    backgroundColor: "indianred",
+    width: 70,
+    height: 30,
+    position: "absolute",
+    top: 50,
+    left: 20,
+    right: 100,
+    borderRadius: 40,
+    elevation: 2,
+    zIndex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 25,
+    fontWeight: "italic",
   },
 });
--- components/WelcomePage.js
+++ components/WelcomePage.js
@@ -1,12 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Alert,
-  Image,
-  TouchableOpacity,
-} from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import flowers from "../assets/flowers.png";
 
 export default function WelcomePage({ navigation }) {
